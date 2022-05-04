f655395e1485fb8870d04665a83d6560ba6f8fd1
pbanic <pbanic@foi.hr>
2022-05-04 21:01:00 +0200

    Third commit

--- page.js
+++ page.js
@@ -1,24 +1,111 @@
+//click on back button
 $(document).ready(function () {
-  $("#PrevAct").on("click", function () { alert("Previous") });
-});
-$(document).ready(function () {
-  $("#NextAct").on("click", function () { alert("Next") });
+  $('#PrevAct').click(function () { shiftSlideTop(1), shiftSlideBottom(1) })
 });
 
+//click on next button
+$(document).ready(function () {
+  $('#NextAct').click(function () { shiftSlideTop(-1), shiftSlideBottom(-1) })
+});
+
+//on hover - back button
 $(document).ready(function () {
   $("#PrevAct").hover(function () {
     $("#BackButton").attr("src", "./Assets/arrow-blue-right.png");
   },
-  function() {
-    $("#BackButton").attr("src", "./Assets/arrow-gray-right.png");
-  })
+    function () {
+      $("#BackButton").attr("src", "./Assets/arrow-gray-right.png");
+    })
 });
 
+//on hover - next button
 $(document).ready(function () {
   $("#NextAct").hover(function () {
     $("#NextButton").attr("src", "./Assets/arrow-blue-left.png");
   },
-  function() {
-    $("#NextButton").attr("src", "./Assets/arrow-gray-left.png");
+    function () {
+      $("#NextButton").attr("src", "./Assets/arrow-gray-left.png");
+    })
+});
+
+
+//var for both top & bottom slider
+var threshold = 150,
+slideWidth = 500,
+dragStart,
+dragEnd
+
+//mutual function
+function dragPos() {
+  return dragEnd - dragStart;
+}
+
+//top slider
+var SlideshowContainerTop = $('#SlideshowContainerTop')
+
+SlideshowContainerTop.on('mousedown', function () {
+  if (SlideshowContainerTop.hasClass('transition')) return;
+  dragStart = pageX;
+  $(this).on('mousemove', function () {
+    dragEnd = pageX;
+    $(this).css('transform', 'translateX(' + dragPos() + 'px)')
+  })
+  $(document).on('mouseup', function () {
+    if (dragPos() > threshold) { return shiftSlideTop(1) }
+    if (dragPos() < -threshold) { return shiftSlideTop(-1) }
+    shiftSlideTop(0);
   })
 });
+
+function shiftSlideTop(direction) {
+  if (SlideshowContainerTop.hasClass('transition')) return;
+  dragEnd = dragStart;
+  $(document).off('mouseup')
+  SlideshowContainerTop.off('mousemove')
+    .addClass('transition')
+    .css('transform', 'translateX(' + (direction * slideWidth) + 'px)');
+  setTimeout(function () {
+    if (direction === 1) {
+      $('.SlidesTop:first').before($('.SlidesTop:last'));
+    } else if (direction === -1) {
+      $('.SlidesTop:last').after($('.SlidesTop:first').fadeOut(500).fadeIn(500));
+    }
+    SlideshowContainerTop.removeClass('transition')
+    SlideshowContainerTop.css('transform', 'translateX(0px)');
+  }, 700)
+}
+
+//bottom slider
+var SlideshowContainerBottom = $('#SlideshowContainerBottom')
+
+SlideshowContainerBottom.on('mousedown', function () {
+  if (SlideshowContainerBottom.hasClass('transition')) return;
+  dragStart = pageX;
+  $(this).on('mousemove', function () {
+    dragEnd = pageX;
+    $(this).css('transform', 'translateX(' + dragPos() + 'px)')
+  })
+  $(document).on('mouseup', function () {
+    if (dragPos() > threshold) { return shiftSlideBottom(1) }
+    if (dragPos() < -threshold) { return shiftSlideBottom(-1) }
+    shiftSlideBottom(0);
+  })
+});
+
+function shiftSlideBottom(direction) {
+  if (SlideshowContainerBottom.hasClass('transition')) return;
+  dragEnd = dragStart;
+  $(document).off('mouseup')
+  SlideshowContainerBottom.off('mousemove')
+    .addClass('transition')
+    .css('transform', 'translateX(' + (direction * slideWidth) + 'px)');
+  setTimeout(function () {
+    if (direction === 1) {
+      $('.SlidesBottom:first').before($('.SlidesBottom:last'));
+    } else if (direction === -1) {
+      $('.SlidesBottom:last').after($('.SlidesBottom:first').fadeOut(500).fadeIn(500));
+    }
+    SlideshowContainerBottom.removeClass('transition')
+    SlideshowContainerBottom.css('transform', 'translateX(0px)');
+  }, 700)
+}
