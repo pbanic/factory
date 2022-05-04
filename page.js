84e7600b374f2fd017038ce73d3c904bc09eb22e
pbanic <pbanic@foi.hr>
2022-05-05 00:30:17 +0200

    Fifth commit

--- page.js
+++ page.js
@@ -1,108 +1,49 @@
-//var for both top & bottom slider
-var threshold = 150;
-var slideWidth = 500;
-var dragStart;
-var dragEnd;
-var SlideshowContainerTop = $("#SlideshowContainerTop");
-var SlideshowContainerBottom = $("#SlideshowContainerBottom");
-
-//click on back button
 $(document).ready(function () {
-  $("#PrevAct").click(function () { shiftSlideTop(1); shiftSlideBottom(1); });
-});
+  $("#PrevAct").click(function () {
+      moveSlideTop(-1);
+    moveSlideBottom(-1);
+  });
+}); //click on back button
 
-//click on next button
 $(document).ready(function () {
-  $("#NextAct").click(function () { shiftSlideTop(-1); shiftSlideBottom(-1); });
-});
+  $("#NextAct").click(function () {
+    moveSlideTop(1);
+    moveSlideBottom(1);
+  });
+}); //click on next button
 
-//on hover - back button
 $(document).ready(function () {
   $("#PrevAct").hover(function () {
     $("#BackButton").attr("src", "./Assets/arrow-blue-right.png");
-  },
-    function () {
-      $("#BackButton").attr("src", "./Assets/arrow-gray-right.png");
-    });
-});
+  }, function () {
+    $("#BackButton").attr("src", "./Assets/arrow-gray-right.png");
+  });
+}); //onHover/offHover - back button
 
-//on hover - next button
 $(document).ready(function () {
   $("#NextAct").hover(function () {
     $("#NextButton").attr("src", "./Assets/arrow-blue-left.png");
-  },
-    function () {
-      $("#NextButton").attr("src", "./Assets/arrow-gray-left.png");
-    });
-});
-
-//mutual function
-function dragPos() {
-  return dragEnd - dragStart;
-}
-
-//top slider
-SlideshowContainerTop.on("mousedown", function () {
-  if (SlideshowContainerTop.hasClass("transition")) return;
-  dragStart = pageX;
-  $(this).on("mousemove", function () {
-    dragEnd = pageX;
-    $(this).css("transform", "translateX(" + dragPos() + "px)");
+  }, function () {
+    $("#NextButton").attr("src", "./Assets/arrow-gray-left.png");
   });
-  $(document).on("mouseup", function () {
-    if (dragPos() > threshold) { return shiftSlideTop(1); }
-    if (dragPos() < -threshold) { return shiftSlideTop(-1); }
-    shiftSlideTop(0);
-  });
-});
+}); //onHover/offHover - next button
 
-function shiftSlideTop(direction) {
-  if (SlideshowContainerTop.hasClass("transition")) return;
-  dragEnd = dragStart;
-  $(document).off("mouseup");
-  SlideshowContainerTop.off("mousemove")
-    .addClass("transition")
-    .css("transform", "translateX(" + (direction * slideWidth) + "px)");
+function moveSlideTop(direction) {
   setTimeout(function () {
-    if (direction === 1) {
+    if (direction === 1) { //next
       $(".SlidesTop:first").before($(".SlidesTop:last").fadeOut(0.2).fadeIn(500));
-    } else if (direction === -1) {
+    } else if (direction === -1) { //back
       $(".SlidesTop:last").after($(".SlidesTop:first").fadeOut(0.2).fadeIn(500));
     }
-    SlideshowContainerTop.removeClass("transition");
-    SlideshowContainerTop.css("transform", "translateX(0px)");
-  }, 700);
-}
+  }, 300);
+} //move slide top 
 
-//bottom slider
-SlideshowContainerBottom.on("mousedown", function () {
-  if (SlideshowContainerBottom.hasClass("transition")) return;
-  dragStart = pageX;
-  $(this).on("mousemove", function () {
-    dragEnd = pageX;
-    $(this).css("transform", "translateX(" + dragPos() + "px)");
-  });
-  $(document).on("mouseup", function () {
-    if (dragPos() > threshold) { return shiftSlideBottom(1); }
-    if (dragPos() < -threshold) { return shiftSlideBottom(-1); }
-    shiftSlideBottom(0);
-  });
-});
-
-function shiftSlideBottom(direction) {
-  if (SlideshowContainerBottom.hasClass("transition")) return;
-  dragEnd = dragStart;
-  $(document).off("mouseup");
-  SlideshowContainerBottom.off("mousemove")
-    .addClass("transition")
-    .css("transform", "translateX(" + (direction * slideWidth) + "px)");
+function moveSlideBottom(direction) {
   setTimeout(function () {
-    if (direction === 1) {
+    if (direction === 1) { //next
       $(".SlidesBottom:first").before($(".SlidesBottom:last").fadeOut(0.2).fadeIn(500));
-    } else if (direction === -1) {
+    } else if (direction === -1) { //back
       $(".SlidesBottom:last").after($(".SlidesBottom:first").fadeOut(0.2).fadeIn(500));
     }
-    SlideshowContainerBottom.removeClass("transition");
-    SlideshowContainerBottom.css("transform", "translateX(0px)");
-  }, 700);
-}
+  }, 300);
+} //move slide bottom 
